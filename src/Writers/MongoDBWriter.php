845ded9f14ab30093a6c544d52321e78739d585f
afi <afi@runtime.pro>
2015-05-28 23:51:15 +0300

    added 2 writers

---
+++ src/Writers/MongoDBWriter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: afi
+ * Date: 5/28/15
+ * Time: 10:35 PM
+ */
+
+namespace Solo\Logger\Writers;
+
+
+class MongoDBWriter {
+
+}
---
+++ src/Writers/NativeMailWriter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: afi
+ * Date: 5/28/15
+ * Time: 11:03 PM
+ */
+
+namespace Solo\Logger\Writers;
+
+
+class NativeMailWriter {
+
+}
