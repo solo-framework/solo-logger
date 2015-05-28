330a1c02cc7789b2545db5a7f88a6a9435b319a9
afi <afi@runtime.pro>
2015-05-28 23:51:47 +0300

    added 2 writers

--- src/Writers/MongoDBWriter.php
+++ src/Writers/MongoDBWriter.php
@@ -9,6 +9,44 @@
 namespace Solo\Logger\Writers;
 
 
-class MongoDBWriter {
+use Solo\Logger\LogRecord;
 
+class MongoDBWriter extends BaseWriter
+{
+	/**
+	 * Connection string
+	 *
+	 * @var string
+	 */
+	public $server = "mongodb://username:password@localhost:27017";
+
+	/**
+	 * Database name
+	 *
+	 * @var string
+	 */
+	public $dbname = "";
+
+	/**
+	 * Collection
+	 *
+	 * @var string
+	 */
+	public $collection = "logs";
+
+	protected $coll = null;
+
+	function write($level, LogRecord $data)
+	{
+		if (!$this->coll)
+		{
+			$this->coll = new \MongoClient($this->server);
+			$db = $this->coll->selectDB($this->dbname);
+			$this->coll = $db->selectCollection($this->collection);
+		}
+
+		unset($data->context);
+		$data = (array)$data;
+		$this->coll->save($data);
+	}
 }
--- src/Writers/NativeMailWriter.php
+++ src/Writers/NativeMailWriter.php
@@ -8,7 +8,36 @@
 
 namespace Solo\Logger\Writers;
 
+use Solo\Logger\Logger;
+use Solo\Logger\LogRecord;
 
-class NativeMailWriter {
+class NativeMailWriter extends BaseWriter
+{
+	/**
+	 * String or rray  of e-mails
+	 *
+	 * @var string|array
+	 */
+	public $to = "";
+	public $subject = "{log-level} {message}";
+	public $from = "";
+	public $contentType = 'text/plain';
+	public $encoding = 'utf-8';
 
+	function write($level, LogRecord $data)
+	{
+		$headers[] = "From: {$this->from}";
+
+		$headers = ltrim(implode("\r\n", $headers) . "\r\n", "\r\n");
+		$headers .= "Content-type: {$this->contentType}; charset={$this->encoding}\r\n";
+
+		if ($this->contentType == "text/html" && false === strpos($headers, "MIME-Version:"))
+			$headers .= 'MIME-Version: 1.0' . "\r\n";
+
+		$this->subject = str_replace("{log-level}", Logger::$levels[$data->level], $this->subject);
+		$this->subject = str_replace("{message}", $data->message, $this->subject);
+
+		foreach ($this->to as $to)
+			mail($to, $this->subject, $data->formatted, $headers);
+	}
 }
